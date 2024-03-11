10376b35306e62a9a398fd19546d1a2239378979
Thu Trang <changchang8788@gmail.com>
2024-03-11 14:34:08 +0700

    Creat infor

--- com/practice/objects/common_objects.py
+++ com/practice/objects/common_objects.py
@@ -8,6 +8,7 @@ import os
 class Common(object):
     base_url = 'https://automationexercise.com/'
     login_url = base_url + 'login'
+    signup_url = base_url + 'signup'
 
 
 class LoginPage(object):
@@ -37,3 +38,41 @@ class RegisterTestPage(object):
     submit_btn = '//*[@id="form"]/div/div/div[3]/div/form/button'
     name_value = 'channg8788'
     email_value = 'Mm333@yobmail.com'
+
+class EnterAccountInforPage(object):
+    mrs_radio = '//*[@id="id_gender2"]' 
+    name_input = '//*[@id="name"]'  
+    email_input = '//*[@id="email"]'
+    password_input = '//*[@id="password"]'
+    day_select = '//*[@id="days"]'
+    month_select = '//*[@id="months"]'
+    year_select = '//*[@id="years"]'
+    signup_checkbox = '//*[@id="newsletter"]'
+    receive_checkbox = '//*[@id="optin"]'
+    firstname_input = '//*[@id="first_name"]'
+    lastname_input = '//*[@id="last_name"]'
+    company_input = '//*[@id="company"]'
+    address1_input = '//*[@id="address1"]'
+    address2_input = '//*[@id="address2"]'
+    country_select = '//*[@id="country"]'
+    state_input = '//*[@id="state"]'
+    city_input = '//*[@id="city"]'
+    zipcode_input = '//*[@id="zipcode"]'
+    mobile_input = '//*[@id="mobile_number"]'
+    create_submit = '//*[@id="form"]/div/div/div/div[1]/form/button'
+    day_value = '25'
+    month_value = 'May'
+    year_value = '1988'
+    password_value = '2510Mtccx'
+    firstname_value = 'Trang'
+    lastname_value = 'Luong Thi Thu'
+    company_value = 'ABC'
+    address1_value = 'CDE'
+    address2_value = 'DFJ'
+    state_value = '10000'
+    city_value = '10000'
+    zipcode_value = '10000'
+    mobile_value = '10000'
+    country_value = 'Canada'
+
+
